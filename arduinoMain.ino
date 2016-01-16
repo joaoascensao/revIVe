262c458895472a26bc4f9ee00e221b4e151622d1
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-16 17:34:41 -0600

    Update arduinoMain.ino

--- arduinoMain.ino
+++ arduinoMain.ino
@@ -1,9 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+  
+  // 
+  
+  // Initialize all pins
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
   
+  // get load cell voltage
+  // call PID
+  // figure out how much to move the motor
 }
