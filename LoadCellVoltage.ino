fc7181949d90b9c80964620491d6c69222a3bd09
zhuzhuchoochoo <xz26@rice.edu>
2016-01-17 14:56:32 -0600

    Update LoadCellVoltage.ino

--- LoadCellVoltage.ino
+++ LoadCellVoltage.ino
@@ -4,43 +4,51 @@
 // Example densities
 const float dens1 = 1.0046; // Saline density @ 22C g/mL
 const int calib = 10000; // Predetermined calibration factor from curve (weight = voltage*calib)
-const int timeread = 5000; // Time between readings (ms) 
 
 // Analog values for calibration
-int loadA = 0; // Lower bound weight kg 
-int analogvalA = 0; // analog reading taken with load A on the load cell 
+int loadA = 0; // Lower bound weight kg
+int analogvalA = 0; // analog reading taken with load A on the load cell
 
-float loadB = 2; // Upper bound weight kg 
+float loadB = 2; // Upper bound weight kg
 int analogvalB = 10; // analog reading taken with load B on the load cell
 
 float sumRate = 0;
 float lenRate = 0;
-float avRate
+float avRate1 = 0;
+float avRate2 = 0;
+const int settime = 5000; // Time between readings in ms
+boolean set1 = true;
 
 // Loop
 void loop() {
   while (tictoc(1000) {
-      int analogval = analogRead(A0); // Reading load cell voltage at A0 pin
-  // Calculating weight in 2 ways (To see which one is more accurate) 
+  int analogval = analogRead(A0); // Reading load cell voltage at A0 pin
+    // Calculating weight in 2 ways (To see which one is more accurate)
     // Method 1 using map for smooth linear calibration
-       float load = map(analogval, analogvalA, analogvalB, loadA, loadB); // Mapping calibration between loadA -> loadB
-       float rate1 = load / dens1;  // Mass -> Volume
-       
-       sumRate += rate1;
-       ++ lenRate;
-       
+    float load = map(analogval, analogvalA, analogvalB, loadA, loadB); // Mapping calibration between loadA -> loadB
+    float rate1 = load / dens1;  // Mass -> Volume
+
+    sumRate += rate1;
+    ++ lenRate;
+
     // Method 2, multiplying by predetermined calibration factor
-      float voltage = analogval * (10.0 / 1023.0); // Assuming 0 - 10V output
-      float rate2 = voltage * calib / dens1;
-  
-   // Printing every X seconds (set above at timeread)
-  //if(millis() > time + timeread){
-  //  Serial.print("FlowRate1: ");Serial.println(rate1);
-  //  Serial.print("FlowRate2: ");Serial.println(rate2);
-  //  time = millis();
+    float voltage = analogval * (10.0 / 1023.0); // Assuming 0 - 10V output
+    float rate2 = voltage * calib / dens1;
+  // Calculate on first run
+    if (set1) { 
+      avRate1 = sumRate / lenRate;
+      sumRate = 0;
+      lenRate = 0;
+      set1 = false;
+      delay(settime); // Delays by time set above
+    }
+  // After 1st loop, runs this every other time
+    else {
+      avRate2 = sumRate / lenRate;
+      sumRate = 0;
+      lenRate = 0;
+      flowrate = (avRate2 - avRate1) / settime / 1000;
+      avRate1 = avRate2; // Sets avRate1 to avRate2 for next cycle of flow rate calculation
+      avRate2 = 0; // Resets avRate2
+    }
   }
-  avRate = sumRate / lenRate;
-  sumRate = 0;
-  lenRate = 0;
-  }
-}
