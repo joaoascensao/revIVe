e5acd1e88de72186d0116fc14dfed458b4f909e9
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-17 13:22:13 -0600

    Create tictoc.ino

---
+++ tictoc.ino
@@ -0,0 +1,24 @@
+boolean b
+b = false;
+
+boolean tictoc (float tMil) {
+float tIni
+
+if (!b){
+  tIni = millis();
+}
+
+boolean tElapse
+if ((millis() - tIni) < tMil) {
+  tElapse = false;
+  b=true;
+}
+else {
+  tElapse = true;
+  b=false;
+}
+
+return tElapse
+
+}
+
