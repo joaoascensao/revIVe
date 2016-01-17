ad987a9575fcc2a649660ab1ce55727bb71ce307
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-17 13:33:13 -0600

    Update LoadCellVoltage.ino

--- LoadCellVoltage.ino
+++ LoadCellVoltage.ino
@@ -18,24 +18,36 @@ void setup() {
   Serial.begin(9600); // initialize serial communication at 9600 bits per second:
 }
 
+float sumRate
+float lenRate
+float avRate
+
+sumRate = 0;
+lenRate = 0;
+
 // Loop
 void loop() {
   int analogval = analogRead(A0); // Reading load cell voltage at A0 pin
-
+  while (tictoc(1000) {
   // Calculating weight in 2 ways (To see which one is more accurate) 
     // Method 1 using map for smooth linear calibration
        float load = map(analogval, analogvalA, analogvalB, loadA, loadB); // Mapping calibration between loadA -> loadB
        float rate1 = load / dens1; 
+       
+       sumRate += rate1;
+       lenRate ++;
        
     // Method 2, multiplying by predetermined calibration factor
       float voltage = analogval * (10.0 / 1023.0); // Assuming 0 - 10V output
       float rate2 = voltage * calib / dens1;
   
    // Printing every X seconds (set above at timeread)
-  if(millis() > time + timeread){
-    Serial.print("FlowRate1: ");Serial.println(rate1);
-    Serial.print("FlowRate2: ");Serial.println(rate2);
-    time = millis();
+  //if(millis() > time + timeread){
+  //  Serial.print("FlowRate1: ");Serial.println(rate1);
+  //  Serial.print("FlowRate2: ");Serial.println(rate2);
+  //  time = millis();
   }
-
+  avRate = sumRate / lenRate;
+  }
+  
 }
