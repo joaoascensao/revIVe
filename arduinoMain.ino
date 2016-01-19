6fc640ac79f015070ac5c7fee5aa14e5e0e9d0f1
gfats <gcf1@rice.edu>
2016-01-19 17:33:16 -0600

    Update arduinoMain.ino

--- arduinoMain.ino
+++ arduinoMain.ino
@@ -10,7 +10,7 @@ void loop() {
   // put your main code here, to run repeatedly: 
   
   // get load cell voltage
-  float flowRate
+  float flowRate;
   flowRate = loadCellFlowRate;
   // call PID
   // figure out how much to move the motor
