851e48628708898442adae6d19c637bf20b1c0ad
zhuzhuchoochoo <xz26@rice.edu>
2016-01-20 13:00:06 -0600

    Update LoadCellVoltage.ino

--- LoadCellVoltage.ino
+++ LoadCellVoltage.ino
@@ -22,8 +22,8 @@ boolean set1 = true;
 
 // Loop
 float loadCellFlowRate () {
-  while (tictoc(1000) {
-  int analogval = analogRead(A0); // Reading load cell voltage at A0 pin
+  while (tictoc(1000)) {
+    int analogval = analogRead(A0); // Reading load cell voltage at A0 pin
     // Calculating weight in 2 ways (To see which one is more accurate)
     // Method 1 using map for smooth linear calibration
     float load = map(analogval, analogvalA, analogvalB, loadA, loadB); // Mapping calibration between loadA -> loadB
@@ -35,25 +35,25 @@ float loadCellFlowRate () {
     // Method 2, multiplying by predetermined calibration factor
     float voltage = analogval * (10.0 / 1023.0); // Assuming 0 - 10V output
     float rate2 = voltage * calib / dens1;
-    }
+  }
   // Calculate on first run
-    if (set1) { 
-      avRate1 = sumRate / lenRate;
-      sumRate = 0;
-      lenRate = 0;
-      set1 = false;
-      delay(settime); // Delays by time set above
-    }
+  if (set1) {
+    avRate1 = sumRate / lenRate;
+    sumRate = 0;
+    lenRate = 0;
+    set1 = false;
+    delay(settime); // Delays by time set above
+  }
   // After 1st loop, runs this every other time
-    else {
-      avRate2 = sumRate / lenRate;
-      sumRate = 0;
-      lenRate = 0;
-      flowrate = (avRate2 - avRate1) / settime / 3600000; // Flowrate mL / hr
-      avRate1 = avRate2; // Sets avRate1 to avRate2 for next cycle of flow rate calculation
-      avRate2 = 0; // Resets avRate2
-      Serial.Println(flowrate);
-      delay(settime);
-}
-return flowrate;
+  else {
+    avRate2 = sumRate / lenRate;
+    sumRate = 0;
+    lenRate = 0;
+    flowrate = (avRate2 - avRate1) / settime / 3600000; // Flowrate mL / hr
+    avRate1 = avRate2; // Sets avRate1 to avRate2 for next cycle of flow rate calculation
+    avRate2 = 0; // Resets avRate2
+    Serial.println(flowrate);
+    delay(settime);
+  }
+  return flowrate;
 }
