d32b71c8c056f307a130f10c659053d946d24695
zhuzhuchoochoo <xz26@rice.edu>
2016-01-20 13:00:44 -0600

    Update tictoc.ino

--- tictoc.ino
+++ tictoc.ino
@@ -1,24 +1,21 @@
-boolean b
-b = false;
+boolean b = false;
 
 boolean tictoc (float tMil) {
-float tIni
+  float tIni;
 
-if (!b){
-  tIni = millis();
+  if (!b){
+    tIni = millis();
+  }
+
+  boolean tElapse = false;
+  if ((millis() - tIni) < tMil) {
+    tElapse = true;
+    b=true;
+  }
+  else {
+    tElapse = false;
+    b =false;
+  }
+
+  return tElapse;
 }
-
-boolean tElapse
-if ((millis() - tIni) < tMil) {
-  tElapse = true;
-  b=true;
-}
-else {
-  tElapse = false;
-  b=false;
-}
-
-return tElapse;
-
-}
-
