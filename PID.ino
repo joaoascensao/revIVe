1d2ac645f8da14dfefdb7e55c149795f05bbc779
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-21 19:19:21 -0600

    Update PID.ino

--- PID.ino
+++ PID.ino
@@ -51,3 +51,35 @@ public:
 		return pid;
 	}
 };
+
+/*
+#define pSENSOR         A1
+#define pCONTROLE       3
+
+PID meuPid(1.0, 0, 0);
+
+void setup() {
+	Serial.begin(9600);
+	
+	pinMode(pSENSOR, INPUT);
+	pinMode(pCONTROLE, OUTPUT);
+}
+
+int controlePwm = 50;
+
+void loop() {
+	
+	// Lê temperatura
+	double temperature = map(analogRead(pSENSOR), 0, 1023, 0, 100);
+	
+	// Manda pro objeto PID!
+	meuPid.addNewSample(temperature);
+	
+	
+	// Converte para controle
+	controlePwm = (meuPid.process() + 50);
+	// Saída do controle
+	analogWrite(pCONTROLE, controlePwm);
+	
+}
+*/
