56ea83c4522f47d3b93028ca0cc8d3c7f6bb473f
zhuzhuchoochoo <xz26@rice.edu>
2016-01-24 12:38:18 -0600

    Update arduinoMain.ino

--- arduinoMain.ino
+++ arduinoMain.ino
@@ -1,17 +1,25 @@
 #include "PID.h"
-stepperPID(0.001,1,0);
+PID stepperPID(0.001,1,0);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
   stepperPID.setSetPoint(538);
-  // Initialize all pins
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
   
+  // get load cell voltage
+  float flowRate;
+  flowRate = loadCellFlowRate();
+  
+  stepperPID.addNewSample(flowRate);
+  // biStepper.step(stepperPID.process())
+  
+  // call PID
+  // figure out how much to move the motor
+}
   // get load cell voltage
   float flowRate;
   flowRate = loadCellFlowRate();
