83995d5675c0d50f7df5ba8300f5bc771a0b8b2b
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-25 21:43:13 -0600

    Update arduinoMain.ino

--- arduinoMain.ino
+++ arduinoMain.ino
@@ -72,7 +72,7 @@ char keys[rows][cols] = {
 };
 byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[cols] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
+Keypad keypad4x4 = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 void setup() {
   // put your setup code here, to run once:
