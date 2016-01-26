fd8e549c9ace2dba12ee0c3dbf3266476866fc60
zhuzhuchoochoo <xz26@rice.edu>
2016-01-25 23:28:13 -0600

    Create PID.h

---
+++ PID.h
@@ -0,0 +1,61 @@
+#ifndef PID_h
+#define PID_h
+
+class PID{
+public:
+  double error;
+  double sample;
+  double lastSample;
+  double kP, kI, kD;      
+  double P, D;
+  double I = 0;
+  double pid;
+  int step;
+  int totSteps = 0;
+  
+  double setPoint;
+  long lastProcess;
+  
+  PID(double _kP, double _kI, double _kD) {
+    kP = _kP;
+    kI = _kI;
+    kD = _kD;
+  }
+  
+  void addNewSample(double _sample){
+    sample = _sample;
+  }
+  
+  void setSetPoint(double _setPoint){
+    setPoint = _setPoint;
+  }
+  
+  double process(){
+    // PID implementation
+    error = setPoint - sample;
+    float deltaTime = (millis() - lastProcess) / 1000.0;
+    lastProcess = millis();
+    
+    //P
+    P = error * kP;
+    
+    //I
+    I = I + (error * kI) * deltaTime;
+    
+    //D
+    // Use better D?
+    D = (lastSample - sample) * kD / deltaTime;
+    lastSample = sample;
+    
+    // Sum of everything
+    if (abs(error)>8) {
+      pid = P + I + D;
+    }
+    
+    step = round(pid);
+    return (step - totSteps);
+    totSteps = step;
+  }
+};
+
+#endif
