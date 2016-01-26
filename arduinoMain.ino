4dc857e92c3e86b0c039854d7bddef2ab7b993b1
joaoascensao <joao.a.ascensao@gmail.com>
2016-01-25 21:40:34 -0600

    Update arduinoMain.ino

--- arduinoMain.ino
+++ arduinoMain.ino
@@ -62,6 +62,18 @@ public:
 
 PID stepperPID(0.001,1,0);
 
+const byte rows = 4; //four rows
+const byte cols = 4; //three columns
+char keys[rows][cols] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'#','0','*','D'}
+};
+byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
