0cd68aeaec8c714f82bbdb676aad0ed4ee5b82df
joaoascensao <joao.a.ascensao@gmail.com>
2016-02-10 14:38:59 -0600

    Create appendValue.ino

---
+++ appendValue.ino
@@ -0,0 +1,14 @@
+void appendValue(char key, int *userVal) {
+
+  int newVal = key - '0';
+  userVal[3] = userVal[2];
+  userVal[2] = userVal[1];
+  userVal[1] = userVal[0];
+  userVal[0] = newVal;
+}
+
+int arrayToNum(int *userVal) {
+  
+  return 1000*userVal[3] + 100*userVal[2] + 10*userVal[1] + userVal[0];
+  
+}
